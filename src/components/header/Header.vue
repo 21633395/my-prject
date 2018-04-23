dbb531ba1767562965327547882a65010b5e6928
liyulong <21633395@qq.com>
2018-04-23 22:42:46 +0800

    init

---
+++ src/components/header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    公共的头部信息
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    poiInfo:{
+      type:Object,
+      default:{}
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
